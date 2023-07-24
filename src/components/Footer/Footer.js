da7cca4127fffa0377f7cfdea667d24baecd9de5
AlexandraNovinskaya <alexandranovinskaya@gmail.com>
2023-07-24 16:12:25 +0300

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import TasksFilterItem from '../TasksFilter/TasksFilter';
+
+export default class Footer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <footer className="footer">
+        <span className="todo-count">{this.props.doneCounter}</span>
+        <TasksFilterItem
+          filterCompleted={this.props.filterCompleted}
+          filterAll={this.props.filterAll}
+          filterActive={this.props.filterActive}
+        />
+        <button onClick={() => this.props.deleteAllCompletedTask(this.props.name)} className="clear-completed">
+          Clear completed
+        </button>
+      </footer>
+    );
+  }
+}
+Footer.defaultProps = {
+  doneCounter: 0,
+};
+Footer.propTypes = {
+  doneCounter: PropTypes.number,
+  deleteAllCompletedTask: PropTypes.func,
+  name: PropTypes.object,
+};
